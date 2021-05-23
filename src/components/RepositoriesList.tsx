33e12e15274e20ba875fa661bff89ff967dad520
h.sukiasyan <h.sukiasyan@gethatch.com>
2021-05-23 13:52:53 +0200

    usetypedselector for fix redux ts

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(data);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -16,6 +21,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading ...</h3>}
+      {!loading && !error && data}
     </div>
   );
 };
