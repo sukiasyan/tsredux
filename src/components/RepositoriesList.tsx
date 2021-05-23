f9be6b534d19966e7f4a3a05df53638211028227
h.sukiasyan <h.sukiasyan@gethatch.com>
2021-05-23 13:14:19 +0200

    initial components setup

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+export default RepositoriesList;
